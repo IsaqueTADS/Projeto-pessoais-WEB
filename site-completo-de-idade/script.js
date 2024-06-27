db7a6aa3569756f961c012583f980dcfc2ed86f4
IsaqueTADS <isaqueofc18@gmail.com>
2024-06-27 19:20:09 -0300

    testes

---
+++ site-completo-de-idade/script.js
@@ -0,0 +1,14 @@
+function reloadpag() {
+  location.reload;
+}
+var username = document.getElementById("user");
+var password = document.querySelector("input#pass");
+var res = document.getElementById("res");
+res.style.color = "red";
+function login() {
+  if (username.value.length == 0) {
+    res.innerHTML = "Nome de usuario invalido";
+  } else if (password.value.length == 0) {
+    res.innerHTML = "Senha invalida";
+  }
+}
