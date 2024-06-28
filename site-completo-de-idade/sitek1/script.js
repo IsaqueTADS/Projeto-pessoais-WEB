0fb2a11accfafe5b4aaa8ecb8a9aa1e55e7908e8
IsaqueTADS <isaqueofc18@gmail.com>
2024-06-28 15:22:08 -0300

    versão final

--- site-completo-de-idade/sitek1/script.js
+++ site-completo-de-idade/sitek1/script.js
@@ -10,7 +10,9 @@ function login() {
     res.innerHTML = "Nome de usuario invalido";
   } else if (password.value.length == 0) {
     res.innerHTML = "Senha invalida";
-  } else {
+  } else if(username.value == "adm" && password.value == "adm123") {
     location.href = "pagina2/main.html";
+  }else{
+    res.innerHTML = "Usuario ou senha errados"
   }
 }
